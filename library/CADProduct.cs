85d7cc66f269019a8e4a35dc0cccff4232c7dbef
onewhite <1135651301@qq.com>
2024-03-21 14:19:17 +0100

    Y1893839: Initialize library

---
+++ library/CADProduct.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    class CADProduct
+    {
+        CADProduct()
+        {
+            ;
+        }
+        bool Create(ENProduct eNProduct)
+        {
+            return false;
+        }
+        bool Update(ENProduct eNProduct)
+        {
+            return false;
+        }
+        bool Delete(ENProduct eNProduct)
+        {
+            return false;
+        }
+        bool read(ENProduct eNProduct)
+        {
+            return false;
+        }
+        bool readFirst(ENProduct eNProduct)
+        {
+            return false;
+        }
+        bool readNext(ENProduct eNProduct)
+        {
+            return false;
+        }
+        bool readPrev(ENProduct eNProduct)
+        {
+            return false;
+        }
+
+    }
+}
---
+++ library/ENProduct.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    public class ENProduct
+    {
+        
+        private string _code;
+        private string _name;
+        private int _amount;
+        private float _price;
+        private int _category;
+        private DateTime _creationDate;
+
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public int Amount { get; set; }
+        public float Price { get; set; }
+        public int Category { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        // methods
+        ENProduct()
+        {
+            ;
+        }
+        ENProduct(string code, string name, int amount, float price, int category, DateTime creationDate)
+        {
+            ;
+        }
+        bool Create()
+        {
+            return false;
+        }
+        bool Update()
+        {
+            return false;
+        }
+        bool delete()
+        {
+            return false;
+        }
+        bool Read()
+        {
+            return false;
+        }
+        bool readFirst()
+        {
+            return false;
+        }
+        bool readNext()
+        {
+            return false;
+        }
+        bool readPrev()
+        {
+            return false;
+        }
+    }
+}
