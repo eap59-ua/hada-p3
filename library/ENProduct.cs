0e9a2b41631b629bab4756e2c5ea2606c72c13d1
liu <yl60@alu.ua.es>
2024-03-25 18:05:22 +0100

    Y1893839X: Started constructor in ENProduct

--- library/ENProduct.cs
+++ library/ENProduct.cs
@@ -8,20 +8,20 @@ namespace library
 {
     public class ENProduct
     {
-        
+        // atribute
         private string _code;
         private string _name;
         private int _amount;
         private float _price;
         private int _category;
         private DateTime _creationDate;
-
-        public string Code { get; set; }
-        public string Name { get; set; }
-        public int Amount { get; set; }
-        public float Price { get; set; }
-        public int Category { get; set; }
-        public DateTime CreationDate { get; set; }
+        // property
+        public string Code {get { return _code; }  set { _code = value; } } 
+        public string Name { get { return _name; } set { _name = value; } }
+        public int Amount { get { return _amount; } set { _amount = value; } }
+        public float Price { get { return _price; } set { _price = value; } }
+        public int Category { get { return _category; } set { _category = value; } }
+        public DateTime CreationDate { get { return _creationDate; } set { _creationDate = value; } }
 
         // methods
         ENProduct()
@@ -30,8 +30,14 @@ namespace library
         }
         ENProduct(string code, string name, int amount, float price, int category, DateTime creationDate)
         {
-            ;
+            Code = code;
+            Name = name;
+            Amount = amount;
+            Price = price;
+            Category = category;
+            CreationDate = creationDate;
         }
+
         bool Create()
         {
             return false;
