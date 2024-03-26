4c0bbbfe481c2907e65fe3861c8ccc2d481f6a5e
onewhite <1135651301@qq.com>
2024-03-27 00:18:15 +0100

    Y1893839X: completed Create, update methods in CADPrudco

--- library/CADProduct.cs
+++ library/CADProduct.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
+using System.Data.SqlClient;
 
 namespace library
 {
@@ -15,17 +16,93 @@ namespace library
             // Obtener la cadena de conexión desde el archivo Web.config
             constring = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
         }
+
+        [Obsolete]
         bool Create(ENProduct eNProduct)
         {
-            return false;
+            try
+            {
+                using (SqlConnection sql = new SqlConnection())
+                {
+                    sql.ConnectionString = constring;
+                    sql.Open();
+                    string query = "INSERT INTO Products (code, name, amount, price, category, creationDate) VALUES (@code, @name, @amount, @price, @category, @creationDate)";
+
+                    using (SqlCommand sqlCMD = new SqlCommand(query, sql))
+                    {
+                        sqlCMD.Parameters.AddWithValue("@code", eNProduct.Code);
+                        sqlCMD.Parameters.AddWithValue("@name", eNProduct.Name);
+                        sqlCMD.Parameters.AddWithValue("@amount", eNProduct.Amount);
+                        sqlCMD.Parameters.AddWithValue("@price", eNProduct.Price);
+                        sqlCMD.Parameters.AddWithValue("@category", eNProduct.Category);
+                        sqlCMD.Parameters.AddWithValue("@creationDate", eNProduct.CreationDate);
+
+                        int sucess_row = sqlCMD.ExecuteNonQuery();
+                        // si se insertó al menos una fila
+                        return sucess_row > 0;
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Product operation has failed. Error: {0}", ex.Message);
+                return false;
+            }
         }
         bool Update(ENProduct eNProduct)
         {
-            return false;
+            try
+            {
+                using (SqlConnection sql = new SqlConnection(constring))
+                {
+                    sql.Open();
+                    string query = "UPDATE Products SET name = @name, amount = @amount, price = @price, category = @category, creationDate = @creationDate WHERE code = @code";
+
+                    using (SqlCommand sqlCMD = new SqlCommand(query, sql))
+                    {
+                        sqlCMD.Parameters.AddWithValue("@code", eNProduct.Code);
+                        sqlCMD.Parameters.AddWithValue("@name", eNProduct.Name);
+                        sqlCMD.Parameters.AddWithValue("@amount", eNProduct.Amount);
+                        sqlCMD.Parameters.AddWithValue("@price", eNProduct.Price);
+                        sqlCMD.Parameters.AddWithValue("@category", eNProduct.Category);
+                        sqlCMD.Parameters.AddWithValue("@creationDate", eNProduct.CreationDate);
+
+                        int sucess_row = sqlCMD.ExecuteNonQuery();
+                        // si se actualizó al menos una fila
+                        return sucess_row > 0;
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                   Console.WriteLine("Product operation has failed. Error: {0}", ex.Message);
+                return false;
+            }
         }
         bool Delete(ENProduct eNProduct)
         {
-            return false;
+            try
+            {
+                using(SqlConnection sql = new SqlConnection(constring))
+                {
+                    sql.Open();
+                    string query = "DELETE FROM Products WHERE code = @code";
+
+                    using (SqlCommand sqlCMD = new SqlCommand(query, sql))
+                    {
+                        sqlCMD.Parameters.AddWithValue("@code", eNProduct.Code);
+                        int sucess_row = sqlCMD.ExecuteNonQuery();
+                        // si se eliminó al menos una fila
+                        return sucess_row > 0;
+                    }
+                }
+            }
+            catch (SqlException ex) 
+            {
+
+                Console.WriteLine("Product operation has failed. Error: {0}", ex.Message);
+                return false;
+            }
         }
         bool read(ENProduct eNProduct)
         {
